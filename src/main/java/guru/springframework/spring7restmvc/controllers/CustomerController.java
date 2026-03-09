1da284a14a37a8f0e35927343e6ac8d857d8cc58
janso <jansoh@ikmail.com>
2026-03-09 21:27:37 +0100

    5. Assignment 5: Assignment - GET Customer

---
+++ src/main/java/guru/springframework/spring7restmvc/controllers/CustomerController.java
@@ -0,0 +1,35 @@
+package guru.springframework.spring7restmvc.controllers;
+
+import guru.springframework.spring7restmvc.model.Beer;
+import guru.springframework.spring7restmvc.model.Customer;
+import guru.springframework.spring7restmvc.services.CustomerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomers() {
+        return customerService.listCustomer();
+    }
+
+    @RequestMapping(value = "{customerId}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable UUID customerId) {
+
+        return customerService.getCustomerById(customerId);
+
+    }
+}
---
+++ src/main/java/guru/springframework/spring7restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,58 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.model.Beer;
+import guru.springframework.spring7restmvc.model.BeerStyle;
+import guru.springframework.spring7restmvc.model.Customer;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final Map<UUID, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        this.customerMap = new HashMap<>();
+
+        Customer customer1 = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .customerName("Hoje")
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .customerName("John")
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .customerName("Derek")
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        this.customerMap.put(customer1.getId(), customer1);
+        this.customerMap.put(customer2.getId(), customer2);
+        this.customerMap.put(customer3.getId(), customer3);
+    }
+
+    @Override
+    public List<Customer> listCustomer() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(UUID customerId) {
+        return customerMap.get(customerId);
+    }
+}
