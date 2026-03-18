74086b809931b98fd6d3e2cc77ecd55141360e1d
janso <jansoh@ikmail.com>
2026-03-18 22:18:02 +0100

    9.101. JPA Services

---
+++ src/main/java/guru/springframework/spring7restmvc/services/BeerServiceJpa.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.mappers.BeerMapper;
+import guru.springframework.spring7restmvc.model.BeerDTO;
+import guru.springframework.spring7restmvc.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJpa implements BeerService {
+
+    private final BeerRepository beerRepository;
+    private final BeerMapper beerMapper;
+
+    @Override
+    public List<BeerDTO> listBeers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID beerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO saveBeer(BeerDTO beer) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beer) {
+
+    }
+}
---
+++ src/main/java/guru/springframework/spring7restmvc/services/CustomerServiceJpa.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.mappers.CustomerMapper;
+import guru.springframework.spring7restmvc.model.CustomerDTO;
+import guru.springframework.spring7restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJpa implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomer() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID customerId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
