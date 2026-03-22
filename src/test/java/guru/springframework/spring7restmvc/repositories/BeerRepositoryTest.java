2b8aaf5747ec6309d3861cad2d37e7043c94cfde
janso <jansoh@ikmail.com>
2026-03-22 22:35:59 +0100

    9.117. Database Constraint Validation

--- src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring7restmvc.repositories;
 
 import guru.springframework.spring7restmvc.entities.Beer;
 import guru.springframework.spring7restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -9,12 +10,28 @@ import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerNameTooLong() {
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            beerRepository.save(Beer.builder()
+                    .beerName("My Beer werer pouj werop  pojwerg opgwfopw e  wop fgwerg op")
+                    .beerStyle(BeerStyle.LAGER)
+                    .upc("323423")
+                    .price(BigDecimal.valueOf(1.99))
+                    .build());
+
+            beerRepository.flush();
+        });
+    }
 
     @Test
     void testSaveBeer() {
