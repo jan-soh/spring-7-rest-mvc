bbfa89151f7a858a233f6e258833b7a8291d5471
janso <jansoh@ikmail.com>
2026-03-22 22:29:58 +0100

    9.116. JPA Validation

--- src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/guru/springframework/spring7restmvc/repositories/BeerRepositoryTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring7restmvc.repositories;
 
 import guru.springframework.spring7restmvc.entities.Beer;
+import guru.springframework.spring7restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,8 +19,13 @@ class BeerRepositoryTest {
     @Test
     void testSaveBeer() {
         Beer savedBeer = beerRepository.save(Beer.builder()
-                        .beerName("My Beer")
+                .beerName("My Beer")
+                .beerStyle(BeerStyle.LAGER)
+                .upc("323423")
+                .price(BigDecimal.valueOf(1.99))
                 .build());
+
+        beerRepository.flush();
 
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
